ad36f20df770aff1f8fe9b5a995b8075344b6750
Hugo Massonnet <hmassonn@student.42.fr>
2017-10-28 12:41:26 +0200

    pour denis

--- csharp/Solution.cs
+++ csharp/Solution.cs
@@ -1,43 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 namespace test_genetique
-{ 
-    public class Solution : IComparable
+{
+    public class Champion : IComparable
     {
-        // public double X { get; private set; }
-        // public double Y { get; private set; }
-        // public ID Id { get; private set; }
-        // public int Nb_succes { get; private set; } 
+        public double X { get; private set; }
+        public double Y { get; private set; }
+        public ID Id { get; private set; }
+        public int Nb_succes { get; private set; }
 
-
-        //          edit
         public string Code { get; private set; }
-        //          edit
 
-
-        // public Solution(double x, double y, ID id)
-        public Solution()
+        // public Champion(double x, double y, ID id)
+        public Champion()
         {
-            // this.X = x;
-            // this.X = y;
-            // this.Id = id;
+            this.X = x;
+            this.X = y;
+            this.Id = id;
             this.Code = "";
 
             Exemple_ecris_en_red_code();
             printf(this.Code);
         }
-           
-        // public int CompareTo(object obj)
-        // {
-        //     if (this.Nb_succes < (obj as Solution).Nb_succes)
-        //         return -1;
-        //     if (this.Nb_succes > (obj as Solution).Nb_succes)
-        //         return 1;
-        //     return 0;
-        // }
 
+        public int CompareTo(object obj)
+        {
+            if (this.Nb_succes < (obj as Champion).Nb_succes)
+                return -1;
+            if (this.Nb_succes > (obj as Champion).Nb_succes)
+                return 1;
+            return 0;
+        }
 
-        //       edit
         //      on cree le red code
         public void Exemple_ecris_en_red_code()
         {
@@ -70,15 +64,75 @@ namespace test_genetique
         }
 
         // les parametres
-        // public string Param(int possibles)
-        // {
-        //     if possibles
-        // }
+        public string Param(int possibles)
+        {
+            if possibles == 1
+				return only_direct();
+			else if possibles == 2
+				return only_indirect();
+			else if possibles == 3
+				return only_register();
+			else if possibles == 4
+				return dir_ind();
+			else if possibles == 5
+				return dir_reg();
+			else if possibles == 6
+				return ind_reg();
+			else if possibles == 7
+				return dir_ind_reg();
+			return "error param";
+        }
+
+		public string only_direct()
+		{
+			valu = random(0, 4294967295);
+
+			return valu.to_str;
+		}
+
+		public string only_indirect()
+		{
+			valu = random(-100, 500);
+
+			return valu.to_str;
+		}
+
+		public string only_register()
+		{
+			valu = random(1, 16);
+
+			return "r" + valu.to_str;
+		}
+
+		public string dir_ind()
+		{
+			return only_direct() || only_indirect();
+		}
+
+		public string dir_reg()
+		{
+			return only_direct() || only_register();
+		}
+
+		public string ind_reg()
+		{
+			return only_indirect() || only_register();
+		}
+
+		public string dir_ind_reg()
+		{
+			return only_direct() || only_indirect() || only_register;
+		}
 
         // les ordres
         public string ft_live()
         {
-            return "live: %1\n";
+			string str = "live: ";
+			string param = add_param(1);
+			str += param;
+			str += "\n";
+
+			return str;
         }
 
         // public string ft_ld()
